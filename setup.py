611ab1f0d40a439b8afffecc8849d764ff96c786
btweinstein <btweinstein@gmail.com>
2015-04-25 12:33:35 -0400

    Created setup.py.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='dutton_interactions',
+    version='0.1',
+    packages=[''],
+    url='https://bitbucket.org/btweinstein/dutton_interactions',
+    license='',
+    author='Bryan Weinstein',
+    author_email='bweinstein@seas.harvard.edu',
+    description='',
+    install_requires=['pandas']
+)
